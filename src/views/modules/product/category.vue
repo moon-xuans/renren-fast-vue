2e0c1c74b798fecc6639c6cca99121471b61eda4
zhouxuan <moon_xuans@163.com>
2023-03-06 15:05:20 +0800

    feat(category):add operate button

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -2,8 +2,22 @@
   <el-tree
     :data="menus"
     :props="defaultProps"
-    @node-click="handleNodeClick"
-  ></el-tree>
+    node-key="catId"
+    show-checkbox
+    :expand-on-click-node="false"
+  >
+    <span class="custom-tree-node" slot-scope="{ node, data }">
+      <span>{{ node.label }}</span>
+      <span>
+        <el-button v-if="node.level <= 2" type="text" size="mini" @click="() => append(data)">
+          Append
+        </el-button>
+        <el-button v-if="node.childNodes.length == 0" type="text" size="mini" @click="() => remove(node, data)">
+          Delete
+        </el-button>
+      </span>
+    </span>
+  </el-tree>
 </template>
 
 <script>
@@ -18,9 +32,6 @@ export default {
     };
   },
   methods: {
-    handleNodeClick(data) {
-      console.log(data);
-    },
     // 获取数据列表
     getMenus() {
       this.$http({
@@ -29,6 +40,13 @@ export default {
       }).then(({ data }) => {
         this.menus = data.data;
       });
+    },
+    append(data) {
+      console.log("append", data)
+    },
+
+    remove(node, data) {
+      console.log("remove", node, data)
     },
   },
   // 创建完成(可以访问到当前this实例)
