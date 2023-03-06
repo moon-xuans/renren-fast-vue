e4678fdd11374c1b3cf7c3e71294f95857feed12
zhouxuan <moon_xuans@163.com>
2023-03-06 17:47:28 +0800

    feat(product):optimize category delete

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -5,14 +5,25 @@
     node-key="catId"
     show-checkbox
     :expand-on-click-node="false"
+    :default-expanded-keys="expandedKey"
   >
     <span class="custom-tree-node" slot-scope="{ node, data }">
       <span>{{ node.label }}</span>
       <span>
-        <el-button v-if="node.level <= 2" type="text" size="mini" @click="() => append(data)">
+        <el-button
+          v-if="node.level <= 2"
+          type="text"
+          size="mini"
+          @click="() => append(data)"
+        >
           Append
         </el-button>
-        <el-button v-if="node.childNodes.length == 0" type="text" size="mini" @click="() => remove(node, data)">
+        <el-button
+          v-if="node.childNodes.length == 0"
+          type="text"
+          size="mini"
+          @click="() => remove(node, data)"
+        >
           Delete
         </el-button>
       </span>
@@ -25,6 +36,7 @@ export default {
   data() {
     return {
       menus: [],
+      expandedKey: [],
       defaultProps: {
         children: "children",
         label: "name",
@@ -42,11 +54,32 @@ export default {
       });
     },
     append(data) {
-      console.log("append", data)
+      console.log("append", data);
     },
-
     remove(node, data) {
-      console.log("remove", node, data)
+      var ids = [data.catId];
+      this.$confirm(`是否删除【${data.name}】菜单`, "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning",
+      })
+        .then(() => {
+          this.$http({
+            url: this.$http.adornUrl("/product/category/delete"),
+            method: "post",
+            data: this.$http.adornData(ids, false),
+          }).then(({ data }) => {
+            this.$message({
+              message: "菜单删除成功",
+              type: "success",
+            });
+            // 刷新出新的菜单
+            this.getMenus();
+            // 设置需要默认展开的菜单
+            this.expandedKey = [node.parent.data.catId];
+          });
+        })
+        .catch(() => {});
     },
   },
   // 创建完成(可以访问到当前this实例)
