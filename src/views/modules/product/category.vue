7d7bbf96d9df10693fa6681dcc93574c81eadb0c
zhouxuan <moon_xuans@163.com>
2023-03-10 15:31:25 +0800

    feat(product):complete api  call

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -143,6 +143,22 @@ export default {
       // 3.当前拖拽节点的最新层级
 
       console.log("updateNodes", this.updateNodes);
+      this.$http({
+        url: this.$http.adornUrl("/product/category/update/sort"),
+        method: "post",
+        data: this.$http.adornData(this.updateNodes, false),
+      }).then(({ data }) => {
+        this.$message({
+          message: "菜单顺序等修改成功",
+          type: "success",
+        });
+        // 刷新出新的菜单
+        this.getMenus();
+        // 设置需要默认展开的菜单
+        this.expandedKey = [pCid];
+        this.updateNodes = [];
+        this.maxLevel = 0;
+      });
     },
     updateChildNodeLevel(node) {
       if (node.childNodes.length > 0) {
